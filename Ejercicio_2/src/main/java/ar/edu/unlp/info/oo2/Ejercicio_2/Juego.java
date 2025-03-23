9c4865980902bb28709fc6ba54ed6d0c17ad49a7
SantiCiolfi <148510092+SantiCiolfi@users.noreply.github.com>
2025-03-23 20:51:14 -0300

    Update Juego.java

--- Ejercicio_2/src/main/java/ar/edu/unlp/info/oo2/Ejercicio_2/Juego.java
+++ Ejercicio_2/src/main/java/ar/edu/unlp/info/oo2/Ejercicio_2/Juego.java
@@ -1,16 +1,1 @@
-package ar.edu.unlp.info.oo2.Ejercicio_2;
 
-public class Juego {
-	
-	public String revisarJugada (Jugada ju1, Jugada ju2) {
-		if(ju1.ganaA(ju2)) {
-			return "Gano el jugador 1";
-		}else {
-			if(ju2.ganaA(ju1)) {
-				return "Gano el jugador 2";
-			}else
-				return "Empate";
-		}
-			
-	}
-}
